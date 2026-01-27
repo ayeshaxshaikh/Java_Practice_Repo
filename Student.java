bfe4e1cf4bc485d05b7054a7bda8b07ec1ba4aec
ayeshaxshaikh <ayesha.shaikh0318@gmail.com>
2026-01-27 23:32:15 +0530

    Object class: toString() and hashCode()

---
+++ Student.java
@@ -0,0 +1,42 @@
+//class Student {
+//    String name;
+//    int rollno;
+//    Student(String name, int rollno) {
+//        this.name = name;
+//        this.rollno = rollno;
+//    }
+//    public String toString() {
+//        return name + " " + rollno;
+//    }
+//    public static void main(String[] args) {
+//        Student s1 = new Student("Sam", 1);
+//        Student s2 = new Student("John", 2);
+//        System.out.println(s1);
+//        System.out.println(s1.toString());
+//        System.out.println(s2);
+//    }
+//}
+
+
+//import java.util.ArrayList;
+//
+//class StringDemo {
+//    public String toString()
+//    {
+//        return "StringDemo";
+//    }
+//    public static void main(String[] args) {
+//        String s = new String("Ayesha");
+//        System.out.println(s);
+//        ArrayList list = new ArrayList();
+//        list.add("A");
+//        list.add("B");
+//        System.out.println(list);
+//        StringDemo s1 = new StringDemo();
+//        System.out.println(s1);
+//    }
+//}
+
+
+
+
